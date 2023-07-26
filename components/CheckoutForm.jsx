3331f6c4ffd98252c3d96adcef1c1299cada0000
Maxwell Yang <maxwelly@stripe.com>
2023-07-26 10:21:56 -0700

    add back address element and button

--- components/CheckoutForm.jsx
+++ components/CheckoutForm.jsx
@@ -16,47 +16,102 @@ export default function CheckoutForm() {
   const [message, setMessage] = React.useState(null);
   const [isLoading, setIsLoading] = React.useState(false);
 
-  const onConfirm = async () => {
-    const clientSecret = new URLSearchParams(window.location.search).get(
-        "payment_intent_client_secret"
-      );
-  
-      if (!clientSecret) {
-        return;
-      }
+  React.useEffect(() => {
+    if (!stripe) {
+      return;
+    }
 
-    const {error} = await stripe.confirmPayment({
-      // `Elements` instance that's used to create the Express Checkout Element.
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+
+    if (!clientSecret) {
+      return;
+    }
+
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          setMessage("Payment succeeded!");
+          break;
+        case "processing":
+          setMessage("Your payment is processing.");
+          break;
+        case "requires_payment_method":
+          setMessage("Your payment was not successful, please try again.");
+          break;
+        default:
+          setMessage("Something went wrong.");
+          break;
+      }
+    });
+  }, [stripe]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js hasn't yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    setIsLoading(true);
+
+    const { error } = await stripe.confirmPayment({
       elements,
-      // `clientSecret` from the created PaymentIntent
-      clientSecret,
       confirmParams: {
-        return_url: 'https://nextjselementsintegration.vercel.app/',
+        // Make sure to change this to your payment completion page
+        return_url: "http://localhost:3000",
       },
-      // Uncomment below if you only want redirect for redirect-based payments.
-      // redirect: 'if_required',
     });
 
-    if (error) {
-      // This point is reached only if there's an immediate error when confirming the payment. Show the error to your customer (for example, payment details incomplete).
+    // This point will only be reached if there is an immediate error when
+    // confirming the payment. Otherwise, your customer will be redirected to
+    // your `return_url`. For some payment methods like iDEAL, your customer will
+    // be redirected to an intermediate site first to authorize the payment, then
+    // redirected to the `return_url`.
+    if (error.type === "card_error" || error.type === "validation_error") {
+      setMessage(error.message);
     } else {
-      // Your customer will be redirected to your `return_url`.
+      setMessage("An unexpected error occurred.");
     }
+
+    setIsLoading(false);
   };
 
-  const options = {
-    wallets: {
-        googlePay: 'always'
+  const paymentElementOptions = {
+    layout: "tabs",
+  };
+
+  const addressElementOptions = {
+    mode: 'billing',
+  }
+
+  const eceOptions = {
+    wallet: {
+        googlePay: 'always',
     }
   }
 
   return (
-    <div id="checkout-page">
-      <ExpressCheckoutElement
-        onConfirm={onConfirm}
-        options={options}
+    <form id="payment-form" onSubmit={handleSubmit}>
+      <LinkAuthenticationElement
+        id="link-authentication-element"
+        onChange={(e) => {
+            setEmail(e.value.email)}
+        }
       />
-      <PaymentElement />
-    </div>
+      <ExpressCheckoutElement id="express-checkout-element" options={eceOptions} />
+      <AddressElement id="address-element" options={addressElementOptions} />
+      <PaymentElement id="payment-element" options={paymentElementOptions} />
+      <button disabled={isLoading || !stripe || !elements} id="submit">
+        <span id="button-text">
+          {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
+        </span>
+      </button>
+      {/* Show any error or success messages */}
+      {message && <div id="payment-message">{message}</div>}
+    </form>
   );
 }
